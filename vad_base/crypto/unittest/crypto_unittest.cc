7b12f1ebe8d9c34309193317bebb5b5ee6220b24
tniuli <tniuli@163.com>
2015-05-20 11:38:23 +0800

    add crypto directory

---
+++ vad_base/crypto/unittest/crypto_unittest.cc
@@ -0,0 +1,22 @@
+// copyright:
+//            (C) SINA Inc.
+//
+//      file: 
+//      desc:
+//    author:
+//     email:
+//      date:
+//
+//    change:
+
+
+#include <gtest/gtest.h>
+#include <vad_base/common/define.h>
+
+TEST(Lock, test1) { 
+
+}
+
+TEST(Lock, test2) { 
+
+}
